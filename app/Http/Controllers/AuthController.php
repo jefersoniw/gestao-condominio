d7e1c740ac209ade2336dd1f3a29c1bcebee070f
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2023-07-10 15:21:17 -0300

    Configurando handler para mostrar os erros de exceções atraves do formrequest. Implementando os metodos unauthorized e register e já fazendo login e exibindo o token jwt.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\Unit;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function unauthorized()
+    {
+        return \response()->json([
+            'error' => 'Não autorizado!',
+        ], 401);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $array = [
+            'error' => ''
+        ];
+
+        DB::beginTransaction();
+
+        try {
+
+            $cpf = str_replace(['.', '-', '/'], '', $request->cpf);
+            $pass = \password_hash($request->password, \PASSWORD_DEFAULT);
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->cpf = $cpf;
+            $user->email = $request->email;
+            $user->password = $pass;
+            if (!$user->save()) {
+                throw new Exception("Erro ao registrar usuário!");
+            }
+
+            $token = auth()->attempt([
+                'cpf' => $cpf,
+                'password' => $request->password,
+            ]);
+
+            if (!$token) {
+                $array['error'] = 'Usuário e/ou Senha Inválidos!';
+
+                return \response()->json($array);
+            }
+
+            $array['token'] = $token;
+
+            $user = auth()->user();
+
+            $array['user'] = $user;
+
+            $properties = Unit::select(['id', 'name'])->where('id_owner', $user['id'])->get();
+
+            $array['user']['properties'] = $properties;
+
+            DB::commit();
+
+            return \response()->json($array);
+        } catch (Exception $error) {
+            DB::rollBack();
+
+            $array['error'] = $error;
+
+            return \response()->json($array);
+        }
+    }
 }
