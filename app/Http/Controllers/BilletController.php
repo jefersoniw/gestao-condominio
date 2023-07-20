ce484bae1f68fd0e4dba130b4933ff53353e1aa2
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2023-07-20 20:55:29 -0300

    Exibição de todos os boletos por unidade.

--- app/Http/Controllers/BilletController.php
+++ app/Http/Controllers/BilletController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Billet;
 use Illuminate\Http\Request;
 
 class BilletController extends Controller
 {
-    //
+    public function getAll(Request $request)
+    {
+        $array = [
+            'error' => ''
+        ];
+
+        $unit = $request->input('unit');
+
+        if (!$unit) {
+            return $array['error'] = 'A unidade é obrigatória';
+        }
+
+        $billets = Billet::where('id_unit', $unit)->get();
+
+        foreach ($billets as $billetKey => $billet) {
+
+            $billets[$billetKey]['fileUrl'] = asset('storage/billet/' . $billet['fileUrl']);
+        }
+
+        $array['billets'] = $billets;
+
+        return $array;
+    }
 }
