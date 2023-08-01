cb4f287c4a22aed80ac46e909849d2e4b3417e54
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2023-08-01 20:16:32 -0300

    Fazendo o metodo getall de foundandlost.

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = [
+            'error' => ''
+        ];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+            ->orderBy('dateCreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+            ->orderBy('dateCreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        foreach ($lost as $lostKey => $lostValue) {
+
+            $lost[$lostKey]['dateCreated'] = date('d/m/Y', \strtotime($lostValue['dateCreated']));
+            $lost[$lostKey]['photo'] = asset('storage/' . $lostValue['photo']);
+        }
+
+        foreach ($recovered as $rectKey => $recValue) {
+
+            $recovered[$rectKey]['dateCreated'] = date('d/m/Y', \strtotime($recValue['dateCreated']));
+            $recovered[$rectKey]['photo'] = asset('storage/' . $recValue['photo']);
+        }
+
+        $array['lost'] = $lost;
+        $array['recovered'] = $recovered;
+
+        return response()->json($array);
+    }
 }
