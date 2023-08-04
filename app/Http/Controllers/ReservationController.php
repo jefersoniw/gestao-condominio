1aea7b4b105a542ce7894fdfd3b1f39f1eb19dc6
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2023-08-04 16:43:35 -0300

    starting implementation getReservations in ResertavionController.

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function getReservations()
+    {
+        $array = [
+            'error' => ''
+        ];
+
+
+
+        return $array;
+    }
 }
