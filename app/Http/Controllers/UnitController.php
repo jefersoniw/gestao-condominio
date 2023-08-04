c12ff5a0c80ded24921050ef3bd01abc35a6a30c
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2023-08-04 11:34:55 -0300

    Adicionando pessoas a unidade.

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddPersonRequest;
 use App\Models\Unit;
 use App\Models\UnitPeople;
 use App\Models\UnitPet;
 use App\Models\UnitVehicle;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UnitController extends Controller
 {
@@ -38,5 +41,53 @@ class UnitController extends Controller
         $array['unit']['vehicles'] = $vehicles;
 
         return response()->json($array);
+    }
+
+    public function addPerson($id, AddPersonRequest $request)
+    {
+        $array = [
+            'error' => ''
+        ];
+
+        DB::beginTransaction();
+
+        try {
+
+            $unit = Unit::find($id);
+
+            if (empty($unit)) {
+                return $array['error'] = 'Unidade Inexistente';
+            }
+
+            $person = new UnitPeople();
+            $person->id_unit = $unit->id;
+            $person->name = $request->name;
+            $person->birthdate = $request->birthdate;
+            if (!$person->save()) {
+                throw new Exception("erro ao salvar pessoas a propriedade");
+            }
+
+            DB::commit();
+
+            $people = UnitPeople::where('id_unit', $id)->get();
+
+            foreach ($people as $key => $person) {
+                $people[$key]['birthdate'] = date('d/m/Y', \strtotime($person['birthdate']));
+            }
+
+            $array['unit'] = $unit;
+            $array['unit']['people'] = $people;
+        } catch (Exception $error) {
+            DB::rollBack();
+
+            $error = [
+                'error' => true,
+                'erro_msg' => $error->getMessage()
+            ];
+
+            return $array['error'] = $error;
+        };
+
+        return $array;
     }
 }
