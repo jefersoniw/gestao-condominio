ab86d393cfd1b25e9bde44d3a8a0eaca515271a9
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2023-08-04 11:20:39 -0300

    Pegando informações da unidade/propriedade.

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\UnitPeople;
+use App\Models\UnitPet;
+use App\Models\UnitVehicle;
 use Illuminate\Http\Request;
 
 class UnitController extends Controller
 {
-    //
+    public function getInfo($id)
+    {
+        $array = [
+            'error' => ''
+        ];
+
+        $unit = Unit::find($id);
+
+        if (empty($unit)) {
+            return $array['error'] = 'Unidade Inexistente';
+        }
+
+        $people = UnitPeople::where('id_unit', $id)->get();
+
+        foreach ($people as $key => $person) {
+
+            $people[$key]['birthdate'] = date('d/m/Y', \strtotime($person['birthdate']));
+        }
+
+        $pets = UnitPet::where('id_unit', $id)->get();
+        $vehicles = UnitVehicle::where('id_unit', $id)->get();
+
+        $array['unit'] = $unit;
+        $array['unit']['people'] = $people;
+        $array['unit']['pets'] = $pets;
+        $array['unit']['vehicles'] = $vehicles;
+
+        return response()->json($array);
+    }
 }
