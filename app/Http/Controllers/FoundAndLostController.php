df00604ed890f528eaaca4df5ec90f33a3b7d441
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2023-08-04 10:04:00 -0300

    ajustes.

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = [
+            'error' => ''
+        ];
+
+
+
+        return response()->json($array);
+    }
 }
